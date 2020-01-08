bb97bc63fd36e2d6ece0bb98d7e21617d77f7ba4
Marta Szargało <marta.szargalo@tlen.pl>
2020-01-08 23:23:56 +0100

    Remove class active

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,16 @@
 
   const select = {
     templateOf: {
-      menuProduct: '#template-menu-product',
+      menuProduct: '#template-menu-product'
     },
     containerOf: {
       menu: '#product-list',
-      cart: '#cart',
+      cart: '#cart'
     },
     all: {
       menuProducts: '#product-list > .product',
       menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
+      formInputs: 'input, select'
     },
     menuProduct: {
       clickable: '.product__header',
@@ -22,70 +22,71 @@
       priceElem: '.product__total-price .price',
       imageWrapper: '.product__images',
       amountWidget: '.widget-amount',
-      cartButton: '[href="#add-to-cart"]',
+      cartButton: '[href="#add-to-cart"]'
     },
     widgets: {
       amount: {
         input: 'input[name="amount"]',
         linkDecrease: 'a[href="#less"]',
-        linkIncrease: 'a[href="#more"]',
-      },
-    },
+        linkIncrease: 'a[href="#more"]'
+      }
+    }
   };
 
   const classNames = {
     menuProduct: {
       wrapperActive: 'active',
-      imageVisible: 'active',
-    },
+      imageVisible: 'active'
+    }
   };
 
   const settings = {
     amountWidget: {
       defaultValue: 1,
       defaultMin: 1,
-      defaultMax: 9,
+      defaultMax: 9
     }
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    menuProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.menuProduct).innerHTML
+    )
   };
 
-  class Product{ //Tworzenie klasy, która zawiera konstruktor wyświetlający wiadomość w konsoli//
-    constructor(id, data){
+  class Product {
+    //Tworzenie klasy, która zawiera konstruktor wyświetlający wiadomość w konsoli//
+    constructor(id, data) {
       const thisProduct = this;
-	  
-	  thisProduct.id = id;
-	  thisProduct.data = data;
-	  
-	  thisProduct.renderInMenu();
-	  
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
       console.log('new Product:', thisProduct);
     }
-	
-	renderInMenu(){
-		const thisProduct = this;
-		
-		/*generate HTML based on template */
-		const generratedHTML = templates.menuProduct(thisProduct.data);
-		
-		/*create element using utils.createElementFromHTML */
-		thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-		
-		/*find menu container */
-		const menuContainer = document.querySelector(select.containerOf.menu);
-		
-		/* add element to menu */
-		menuContainer.appendChild(thisProduct.element);
-	}
-	
-  }
-  const app = { //Tworzenie pierwszej instancji klasy Product. Metoda obiektu app będzie tworzyć instancje klasy Product//
-    initMenu: function(){
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      /*generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /*create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /*find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initMenu() {
       const thisApp = this; //Instancja dla każdego produktu. Sprawdzenie, czy dane są gotowe do użycia//
       console.log('thisApp.data:', thisApp.data); //-||-//
-      
+
       const testProduct = new Product();
       console.log('testProduct:', testProduct);
 
@@ -93,17 +94,16 @@
       //console.log('thisApp.data:', thisApp.data);
       //for(let productData in thisApp.data.products){
       //  new Product(productData, thisApp.data.products[productData]);
-        
-      }
-    },
-    
-	initData: function(){ //Instancja dla każdego produktu. Aplikacja ma korzystać z dataSource w pliku data.js jako źródła danych, ale w przyszłości dane będą wczytywane z serwera. Metoda app.initData w przyszłości pozwoli zmienić sposób pobierania danych//
+    }
+
+    initData() {
+      //Instancja dla każdego produktu. Aplikacja ma korzystać z dataSource w pliku data.js jako źródła danych, ale w przyszłości dane będą wczytywane z serwera. Metoda app.initData w przyszłości pozwoli zmienić sposób pobierania danych//
       const thisApp = this; //-||-//
-  
+
       thisApp.data = dataSource; //-||-//
-    },
-    
-	init: function(){
+    }
+
+    init() {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
@@ -113,9 +113,9 @@
 
       thisApp.initData(); //Instancja dla każdego produktu//
       thisApp.initMenu();
-    },
-  };
-  
+    }
+  }
 
-  app.init();
+  const temp = new Product();
+  console.log('TCL: Product -> temp', temp);
 }
