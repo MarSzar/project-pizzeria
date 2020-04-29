c443e2491d8b21bd63a989434b9c5a1c7feb92e8
Marta Szargało <marta.szargalo@tlen.pl>
2020-04-29 22:56:47 +0200

    Data download

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {select, templates} from '../settings.js';
+import {select, templates, settings} from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -9,6 +10,67 @@ export class Booking {
     
     thisBooking.render(widgetBooking);
     thisBooking.initWidgets();
+    thisBooking.getData();  //metoda getData będzie pobierać dane z API używając adresów z parametrami filtrującymi wyniki. Do tego potrzebne są trzy adresy
+  
+    // console.log('thisBooking', thisBooking);
+    // console.log('widgetBooking', widgetBooking);
+  }
+
+  getData(){
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {  //obiekt params (aby ułatwić przygotowanie parametrów filtrujacych wynik zapytania. Do każdego z adresów - tablice
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [    //wydarzenia jednorazowe
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,      
+      ],
+      eventsRepeat: [     //wydarzenia cykliczne
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    //console.log('getData params', params);
+
+    const urls = {  //obiekt z trzema właściwościami, po jednej dla każdego z adresów:
+      booking:            settings.db.url + '/' + settings.db.booking 
+                                          + '?' + params.booking.join('&'),                 //będzie zawierać adres endpointu API, który zwróci listę rezerwacji
+      eventsCurrent:      settings.db.url + '/' + settings.db.event   
+                                          + '?' + params.eventsCurrent.join('&'),          //zwróci listę wydarzeń jednorazowych
+      eventsRepeat:       settings.db.url + '/' + settings.db.event   
+                                          + '?' + params.eventsRepeat.join('&'),          //zwróci listę wydarzeń cyklicznych
+    };
+
+    //console.log('getData urls', urls);
+
+    Promise.all([
+      fetch(urls.booking),      //funkcja fetch połączy się z serwerem API (pozwoli uruchomić kolejną metodę, dopiero kiedy wszystkie trzy pytania zwrócą odpowiedzi)
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),    
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
   }
 
   render(widgetBooking){    
